95a271d40513962451419417c5a39d1c555b3786
Jorge Zepeda <JorgeA.Zepeda@alumno.udg.mx>
2023-05-06 03:37:08 -0600

    Add, Delete

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <script setup>
+
+
 </script>
 
 <template>
   <header>
 
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">
+          <img src="./components/icons/logo.ico" alt="" width="30" height="24" class="d-inline-block align-text-top">
+          Heroes
+        </router-link>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/"> Home </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/coleccion"> Cromos </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/add"> add </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/list"> list </router-link>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+
   </header>
 
   <main>
+    <Suspense>
+      <RouterView></RouterView>
+    </Suspense>
 
   </main>
 </template>
 
 <style scoped>
-
+header {
+  margin-bottom: 80px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,31 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBO2Np4CtWwVmtkpzGiQF7ym4zL71DqoCc",
+  authDomain: "vue-cromes.firebaseapp.com",
+  projectId: "vue-cromes",
+  storageBucket: "vue-cromes.appspot.com",
+  messagingSenderId: "227159083803",
+  appId: "1:227159083803:web:feab580a283f20ae78a1be"
+};
+
+// Initialize Firebase
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const app = createApp(App)
+
+
+app.use(router)
+app.mount('#app')
+
